c3795888ccfa18fd6a2f0c83e709820a040fdbb3
Alexandre Petit <AlexandrePetit@users.noreply.github.com>
2018-04-16 21:15:49 +0200

    Removed image upload on new form actions for MetalMaiden and Nation.

--- src/Form/MetalMaidenType.php
+++ src/Form/MetalMaidenType.php
@@ -9,6 +9,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MetalMaidenType extends AbstractType
@@ -34,6 +36,18 @@ class MetalMaidenType extends AbstractType
                 'required' => false,
             ))
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $metalMaiden = $event->getData();
+            $form = $event->getForm();
+
+            // checks if the MetalMaiden object is "new"
+            // If no data is passed to the form, the data is "null".
+            // This should be considered a new "MetalMaiden"
+            if (!$metalMaiden || null === $metalMaiden->getId()) {
+                $form->remove('portraitImageFile'); // Prevent error with filename not yet existing
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Form/NationType.php
+++ src/Form/NationType.php
@@ -7,6 +7,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class NationType extends AbstractType
@@ -19,6 +21,18 @@ class NationType extends AbstractType
                 'required'    => false
             ))
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $nation = $event->getData();
+            $form = $event->getForm();
+
+            // checks if the nation object is "new"
+            // If no data is passed to the form, the data is "null".
+            // This should be considered a new "nation"
+            if (!$nation || null === $nation->getId()) {
+                $form->remove('imageFile'); // Prevent error with filename not yet existing
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)

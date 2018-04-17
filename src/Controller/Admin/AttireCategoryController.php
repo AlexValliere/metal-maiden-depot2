7671113667e960c7d6821880062b7ed929f3e7f6
Alexandre Petit <AlexandrePetit@users.noreply.github.com>
2018-04-17 23:01:08 +0200

    Preparation for controller moves, part 2.

---
+++ src/Controller/Admin/AttireCategoryController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\AttireCategory;
+use App\Form\AttireCategoryType;
+use App\Repository\AttireCategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/attire-category")
+ */
+class AttireCategoryController extends Controller
+{
+    /**
+     * @Route("/", name="admin_attire_category_index", methods="GET")
+     */
+    public function index(AttireCategoryRepository $attireCategoryRepository): Response
+    {
+        return $this->render('admin/attire_category/index.html.twig', ['attire_categories' => $attireCategoryRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="admin_attire_category_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $attireCategory = new AttireCategory();
+        $form = $this->createForm(AttireCategoryType::class, $attireCategory);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($attireCategory);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_attire_category_index');
+        }
+
+        return $this->render('admin/attire_category/new.html.twig', [
+            'attire_category' => $attireCategory,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_attire_category_show", methods="GET")
+     */
+    public function show(AttireCategory $attireCategory): Response
+    {
+        return $this->render('admin/attire_category/show.html.twig', ['attire_category' => $attireCategory]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin_attire_category_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, AttireCategory $attireCategory): Response
+    {
+        $form = $this->createForm(AttireCategoryType::class, $attireCategory);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_attire_category_edit', ['id' => $attireCategory->getId()]);
+        }
+
+        return $this->render('admin/attire_category/edit.html.twig', [
+            'attire_category' => $attireCategory,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_attire_category_delete", methods="DELETE")
+     */
+    public function delete(Request $request, AttireCategory $attireCategory): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$attireCategory->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($attireCategory);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_attire_category_index');
+    }
+}
---
+++ src/Controller/Admin/NationController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Nation;
+use App\Form\NationType;
+use App\Repository\NationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/nation")
+ */
+class NationController extends Controller
+{
+    /**
+     * @Route("/", name="admin_nation_index", methods="GET")
+     */
+    public function index(NationRepository $nationRepository): Response
+    {
+        return $this->render('admin/nation/index.html.twig', ['nations' => $nationRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="admin_nation_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $nation = new Nation();
+        $form = $this->createForm(NationType::class, $nation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($nation);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_nation_index');
+        }
+
+        return $this->render('admin/nation/new.html.twig', [
+            'nation' => $nation,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_nation_show", methods="GET")
+     */
+    public function show(Nation $nation): Response
+    {
+        return $this->render('admin/nation/show.html.twig', ['nation' => $nation]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin_nation_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Nation $nation): Response
+    {
+        $form = $this->createForm(NationType::class, $nation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_nation_edit', ['id' => $nation->getId()]);
+        }
+
+        return $this->render('admin/nation/edit.html.twig', [
+            'nation' => $nation,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_nation_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Nation $nation): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$nation->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($nation);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_nation_index');
+    }
+}

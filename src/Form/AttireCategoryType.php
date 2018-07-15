ec7cab9c0432e46956f712d6418723ceb441a985
Alexandre Petit <AlexandrePetit@users.noreply.github.com>
2018-07-15 23:41:41 +0200

    Added more translations. Transformed more link into buttons in admin dashboard area.

--- src/Form/AttireCategoryType.php
+++ src/Form/AttireCategoryType.php
@@ -12,8 +12,12 @@ class AttireCategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('abbreviation')
-            ->add('name')
+            ->add('abbreviation', null, array(
+                'label_format' => 'form.attire_category.%name%',
+            ))
+            ->add('name', null, array(
+                'label_format' => 'form.attire_category.%name%',
+            ))
         ;
     }
 
